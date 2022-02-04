c8c685ea97339e82942666fdd306371c34403c89
Shen Zhou Hong <ShenZhouHong@users.noreply.github.com>
2022-02-04 18:00:39 -0500

    Updated wordcount, added wordcount.sh utility
    Quick update of the overall word count. Also added the `wordcount.sh`
    utility script into version control.

---
+++ latex/sections/wordcount.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Counts the total number of pages using texcount
+/usr/bin/texcount -total \
+	introduction.tex \
+	cosmogony.tex \
+	contingency.tex \
+	teleology.tex \
+	conclusion.tex
